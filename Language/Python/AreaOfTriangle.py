73d41449e47816c3609608482b03ff290e2e34d8
Mishrak <mishraksharma1991@gmail.com>
2020-08-07 12:14:33 -0400

    Added Area of Triangle program

---
+++ Language/Python/AreaOfTriangle.py
@@ -0,0 +1,7 @@
+l_base = 0.0
+l_height = 0.0
+l_area = 0.0
+l_base = float(input('Enter the base value of the triangle = '))
+l_height = float(input('Enter the height value of the triangle = '))
+l_area = 0.5 * l_base * l_height
+print('Area of triangle = ', l_area)
